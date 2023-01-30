9905df1cefd927b6d5a71cf180029edade0a556a
e-pgsa <pasang.sherpa@gft.com>
2023-01-30 21:06:41 +0000

    starting templates

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello this is the main page <h1>Hello<h1>"
+    return "Hello this is the main page <h1>Hello</h1>"
 
-@app.route("/<name>") #This lets us append /"string" at the end of our URL which is then passed on to our function as a parameter. 
-def user(name):
-    return f"Hello {name}!"
+# @app.route("/<name>") #This lets us append /"string" at the end of our URL which is then passed on to our function as a parameter. 
+# def user(name):
+#     return f"Hello {name}!"
 
-@app.route("/admin")
-def admin():
-    return redirect(url_for("home")) #This redirects you to a specified page. Here de define the name of the function home so that this redirects us there. 
+# @app.route("/admin/") # The extra / after "admin" allows the web browser to hit this route with or without the extra slash. 
+# def admin():
+#     return redirect(url_for("user", name="Admin")) #This redirects you to a specified page. Here de define the name of the function home so that this redirects us there.
+
+
 
 if __name__=="__main__":
     app.run()
