9e28aecbca2c4860c5476d7e55fb582cb6a23619
e-pgsa <pasang.sherpa@gft.com>
2023-01-30 21:19:24 +0000

    passing variables from html file to main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello this is the main page <h1>Hello</h1>"
+@app.route("/<name>")
+def home(name):
+    return render_template("index.html", content=name)
 
 # @app.route("/<name>") #This lets us append /"string" at the end of our URL which is then passed on to our function as a parameter. 
 # def user(name):
