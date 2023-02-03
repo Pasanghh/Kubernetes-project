890ec86f7f6cbe50366d071c9d0bb571227e9399
e-pgsa <pasang.sherpa@gft.com>
2023-02-03 12:52:36 +0000

    terraform files

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+        source = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+    project = "golden-object-376612"
+    region = "eurpoe-west2"
+    
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "gcp_region" {
+  type = string
+  description = "GCP Region"
+}
+
+variable "gcp_project" {
+    type = string
+    description = "GCP Project Name"
+}
+
+variable "gcp_region" {
+    type = string
+    description = "GCP Region"  
+}
+
+variable "bucket-name" {
+  type = string
+  description = "Terraform Backend Bucket"
+}
